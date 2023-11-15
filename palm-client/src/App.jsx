1a0283773293d7c8148cdabfe7d087477e07c986
Eduardo Cortez Valente <130951926+EduardoCorVal@users.noreply.github.com>
2023-11-15 11:59:16 -0600

    feature(chat.css): styles added

--- palm-client/src/App.jsx
+++ palm-client/src/App.jsx
@@ -1,16 +1,21 @@
 import { useState } from "react";
+import "./chat.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <main>
-      <h1>Hello, world!</h1>
-      <article/>
-        <div>
-          <textarea/>
-          <button>Send</button>
+    <main className="main-container">
+      <h1 className="h1-chat">Hello, world!</h1>
+      <div className="div-chat">
+        <div className="max-width-height">
+          <article style={{ margin: '0' }}/>
         </div>
+      </div>
+      <div className="div-text-area">
+        <textarea className="textarea" placeholder="Ingresa tu prompt..."/>
+        <button className="button">Send</button>
+      </div>
     </main>
   );
 }
