2315339dd50f3d9264485de8e7e763455b9f4005
Eduardo Cortez Valente <130951926+EduardoCorVal@users.noreply.github.com>
2023-11-15 13:36:21 -0600

    fix(app): Loading and enter added

--- palm-client/src/App.jsx
+++ palm-client/src/App.jsx
@@ -1,45 +1,72 @@
-import { useState, useEffect } from "react";
+import { useState, useRef } from "react";
 import DOMPurify from "dompurify";
 import { marked } from "marked";
+import "./chat.css";
 
 marked.use({
   gfm: true,
-})
-
-// Styles
-import "./chat.css";
+});
 
 function App() {
-  const [serverData, setServerData] = useState('');
-  const [userPrompt, setUserPrompt] = useState('');
+  const [serverData, setServerData] = useState([{}]);
+  const [userPrompt, setUserPrompt] = useState("");
+  const inputRef = useRef(null);
+
+  function handleKeyDown(e) {
+    if (e.key === "Enter" && !e.shiftKey) {
+      e.preventDefault();
+      inputRef.current.blur();
+      handleSubmit();
+    }
+  }
 
   function handleSubmit() {
-    fetch("/api", {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ 'prompt': userPrompt }),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        const html = DOMPurify.sanitize(marked(data));
-        setServerData({ ...data, html});
-        console.log(serverData);
-      });
+    setServerData("");
+    if (userPrompt !== "") {
+      fetch("/api", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ prompt: userPrompt }),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          const html = DOMPurify.sanitize(marked(data));
+          setServerData({ ...data, html });
+          inputRef.current.focus();
+          setUserPrompt("");
+        });
+    }
   }
 
   return (
     <main className="main-container">
-      <h1 className="h1-chat">Hello, world!</h1>
+      <h1 className="h1-chat">MyPrompter</h1>
       <div className="div-chat">
         <div className="max-width-height">
-          <article style={{ margin: "0" }} dangerouslySetInnerHTML={{ __html: serverData.html }} />
+          {serverData === "" ? (
+            "Loading..."
+          ) : (
+            <article
+              style={{ margin: "0" }}
+              dangerouslySetInnerHTML={{ __html: serverData.html }}
+            />
+          )}
         </div>
       </div>
       <div className="div-text-area">
-        <textarea onChange={(e) => setUserPrompt(e.target.value)} className="textarea" placeholder="Ingresa tu prompt..." />
-        <button onClick={handleSubmit} className="button">Send</button>
+        <textarea
+          onChange={(e) => setUserPrompt(e.target.value)}
+          onKeyDown={handleKeyDown}
+          ref={inputRef}
+          value={userPrompt}
+          className="textarea"
+          placeholder="Type in Prompt..."
+        />
+        <button onClick={handleSubmit} className="button">
+          Submit
+        </button>
       </div>
     </main>
   );
