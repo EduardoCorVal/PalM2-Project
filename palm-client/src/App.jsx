bae1712fb0222aafce152de8c0766fbaf32a4274
Eduardo Cortez Valente <130951926+EduardoCorVal@users.noreply.github.com>
2023-11-15 13:12:30 -0600

    feature(all): Working as expected

--- palm-client/src/App.jsx
+++ palm-client/src/App.jsx
@@ -1,28 +1,45 @@
 import { useState, useEffect } from "react";
+import DOMPurify from "dompurify";
+import { marked } from "marked";
+
+marked.use({
+  gfm: true,
+})
+
+// Styles
 import "./chat.css";
 
 function App() {
-  const [serverData, setServerData] = useState({});
-  useEffect(() => {
-    fetch("/api")
+  const [serverData, setServerData] = useState('');
+  const [userPrompt, setUserPrompt] = useState('');
+
+  function handleSubmit() {
+    fetch("/api", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ 'prompt': userPrompt }),
+    })
       .then((res) => res.json())
       .then((data) => {
-        setServerData(data);
-        console.log(data);
+        const html = DOMPurify.sanitize(marked(data));
+        setServerData({ ...data, html});
+        console.log(serverData);
       });
-  }, []);
+  }
 
   return (
     <main className="main-container">
       <h1 className="h1-chat">Hello, world!</h1>
       <div className="div-chat">
         <div className="max-width-height">
-          <article style={{ margin: "0" }}>{serverData}</article>
+          <article style={{ margin: "0" }} dangerouslySetInnerHTML={{ __html: serverData.html }} />
         </div>
       </div>
       <div className="div-text-area">
-        <textarea className="textarea" placeholder="Ingresa tu prompt..." />
-        <button className="button">Send</button>
+        <textarea onChange={(e) => setUserPrompt(e.target.value)} className="textarea" placeholder="Ingresa tu prompt..." />
+        <button onClick={handleSubmit} className="button">Send</button>
       </div>
     </main>
   );
