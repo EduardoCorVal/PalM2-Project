72e21c7e34e8b001e74864ae785a3991916cc69b
Eduardo Cortez Valente <130951926+EduardoCorVal@users.noreply.github.com>
2023-11-15 12:12:14 -0600

    feature(ChatApp): Connection with backend done

--- palm-client/src/App.jsx
+++ palm-client/src/App.jsx
@@ -1,19 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./chat.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [serverData, setServerData] = useState({});
+  useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => {
+        setServerData(data);
+        console.log(data);
+      });
+  }, []);
 
   return (
     <main className="main-container">
       <h1 className="h1-chat">Hello, world!</h1>
       <div className="div-chat">
         <div className="max-width-height">
-          <article style={{ margin: '0' }}/>
+          <article style={{ margin: "0" }}>{serverData}</article>
         </div>
       </div>
       <div className="div-text-area">
-        <textarea className="textarea" placeholder="Ingresa tu prompt..."/>
+        <textarea className="textarea" placeholder="Ingresa tu prompt..." />
         <button className="button">Send</button>
       </div>
     </main>
